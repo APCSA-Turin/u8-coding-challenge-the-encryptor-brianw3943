13b20130ae346681ba0a0d4fff2c702794a7157e
brianw3943 <brianw124@nycstudents.net>
2025-03-11 16:26:48 +0000

    commit

--- src/main/java/com/example/project/Encryptor.java
+++ src/main/java/com/example/project/Encryptor.java
@@ -5,19 +5,67 @@ import java.util.Arrays;
 public class Encryptor {
     
     public static int determineColumns(int messageLen, int rows){
-        
-        return 0;
+        int columns = 0;
+        for (int i = 0; i * rows < messageLen; i++) {
+            columns++;
+        }
+        if (messageLen == 0) {
+            columns = 1;
+        }
+        return columns;
     }
     
     public static String[][] generateEncryptArray(String message, int rows) {
-        return new String [0][0];
+        String[][] encryptedArray = new String [rows][determineColumns(message.length(), rows)];
+        int index = 0;
+        for (int i = 0; i < encryptedArray.length; i++) {
+            for (int j = 0; j < encryptedArray[i].length; j++) {
+                if (index > message.length() - 1) {
+                    encryptedArray[i][j] = "=";
+                } else {
+                    encryptedArray[i][j] = message.substring(index, index + 1);
+                }
+                index++;
+            }
+        }
+        return encryptedArray;
     }
 
     public static String encryptMessage(String message, int rows){
-        return "";
+        String[][] encryptedArray = generateEncryptArray(message, rows);
+        String encryptedMessage = "";
+        for (int j = encryptedArray[0].length - 1; j >= 0; j--) {
+            for (int i = 0; i < encryptedArray.length; i++) {
+                String charString = encryptedArray[i][j];
+                encryptedMessage += charString;
+            }
+        }
+        return encryptedMessage;
     }
 
     public static String decryptMessage(String encryptedMessage, int rows) {
-        return "";
+        int columns = determineColumns(encryptedMessage.length(), rows);
+        int index = 0;
+        String decryptedString = "";
+        String[][] decryptedMessage = new String[rows][columns];
+        for (int i = columns - 1; i >= 0; i--) {
+            for (int j = 0; j < rows; j++) {
+                decryptedMessage[j][i] = encryptedMessage.substring(index, index + 1);
+                index++;
+            }
+        }
+        for (int i = 0; i < decryptedMessage.length; i++) {
+            for (int j = 0; j < decryptedMessage[i].length; j++) {
+                decryptedString += decryptedMessage[i][j];
+            }
+        }
+        for (int i = decryptedString.length() - 1; i >= 0; i++) {
+            if (decryptedString.substring(i, i + 1) == "=") {
+                decryptedString = decryptedString.substring(0, decryptedString.length() - 1);
+            } else {
+                break;
+            }
+        }
+        return decryptedString;
     }
 }
